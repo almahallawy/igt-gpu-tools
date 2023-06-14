e502334f9f3eca2bb3b8733fa554311a6f70183d
Mauro Carvalho Chehab <mchehab@kernel.org>
2023-06-14 12:58:10 +0200

    tests/xe: Add a test that launches the xe driver live kunit tests
    The xe driver live kunit tests live in the xe driver tests/
    subdirectory and are intended to be launched by this igt test.
    
    To begin with we only have one test ("dma-buf").
    
    [mchehab: updated to reflect current KUnit tests]
    
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Signed-off-by: Dominik Karol Piątkowski <dominik.karol.piatkowski@intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

---
+++ tests/xe/xe_live_ktest.c
@@ -0,0 +1,52 @@
+#include "igt.h"
+#include "igt_kmod.h"
+
+/**
+ * TEST: Xe driver live kunit tests
+ * Description: Xe driver live dmabuf unit tests
+ * Category: Software building block
+ * Sub-category: kunit
+ * Functionality: kunit
+ * Test category: functionality test
+ * Run type: BAT
+ *
+ * SUBTEST: bo
+ * Functionality: bo
+ *
+ * SUBTEST: dmabuf
+ * Functionality: dmabuf
+ *
+ * SUBTEST: migrate
+ * Functionality: migrate
+ *
+ * SUBTEST: pci
+ * Functionality: pci
+ *
+ * SUBTEST: rtp
+ * Functionality: rtp
+ *
+ * SUBTEST: wa
+ * Functionality: workarounds
+ */
+
+struct kunit_tests {
+	const char *kunit;
+	const char *name;
+};
+
+static const struct kunit_tests live_tests[] = {
+	{ "xe_bo_test",		"bo" },
+	{ "xe_dma_buf_test",	"dmabuf" },
+	{ "xe_migrate_test",	"migrate" },
+	{ "xe_pci_test",	"pci" },
+	{ "xe_rtp_test",	"rtp" },
+	{ "xe_wa_test",		"wa" },
+};
+
+igt_main
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(live_tests); i++)
+		igt_kunit(live_tests[i].kunit, live_tests[i].name, NULL);
+}
