2062f3860dbdd1062f54f925cf6b49e28561c75a
Mauro Carvalho Chehab <mchehab@kernel.org>
2023-06-19 11:49:48 +0200

    tests/xe/xe_spin_batch: fix test documentation
    The test documentation is wrong:
    - it should have just one TEST: tag with the contents of the
      test file;
    - it is missing Category, Sub-category, Functionality and
      Test category fields. Those are needed for proper grafana
      functionality mapping.
    
    Reviewed-by: Sai Gowtham Ch <sai.gowtham.ch@intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- tests/xe/xe_spin_batch.c
+++ tests/xe/xe_spin_batch.c
@@ -4,11 +4,17 @@
 #include "xe/xe_ioctl.h"
 #include "xe/xe_query.h"
 
+/**
+ * TEST: Tests for spin batch submissons.
+ * Category: Software building block
+ * Sub-category: spin
+ * Functionality: intel-bb
+ * Test category: functionality test
+ */
+
 #define MAX_INSTANCE 9
 
 /**
- * TEST: Basic test for spin batch submissons.
- *
  * SUBTEST: spin-basic
  * Description: Basic test to submit spin batch submissons on copy engine.
  * Run type: FULL
@@ -29,8 +35,6 @@ static void spin_basic(int fd)
 }
 
 /**
- * TEST:Test for spin batch submissons.
- *
  * SUBTEST: spin-batch
  * Description: Create vm and engine of hwe class and run the spinner on it.
  * Run type: FULL
@@ -59,8 +63,6 @@ static void spin(int fd, struct drm_xe_engine_class_instance *hwe)
 }
 
 /**
- * TEST: Basic test for spin batch submission on all hwe.
- *
  * SUBTEST: spin-basic-all
  * Description: Basic test which validates the functionality of spinner on all hwe.
  * Run type: FULL
@@ -94,7 +96,6 @@ static void spin_basic_all(int fd)
 }
 
 /**
- * TEST: Test for spin batch submissions.
  * SUBTEST: spin-all
  * Description: Spinner test to run on all the engines!
  * Run type: FULL
