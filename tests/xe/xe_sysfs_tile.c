e78963553e05a2413cf735824517547b2bb19936
Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>
2023-07-10 10:13:30 +0530

    tests/xe/xe_sysfs_tile: adds new test to verify per tile vram addr_range
    For each tile the test reads the sysfs entry physical_vram_size_bytes
    and compares the value with vram size exposed from query ioctl.
    
    v2:
    - Change sysfs entry name. (Tejas)
    - Change test name to xe_sysfs_tile_prop. (Rahul)
    
    v3:
    - Rectify assertion condition.
    
    v4:
    - use igt_assert_lt_u64 instead of igt_assert_lt
    for comparing u64.
    
    v5:
    - update xe_for_each_tile call.
    
    v6:
    - Rename testcase to xe_sysfs_tile
    - Pass tilefd and use it in igt_sysfs_scanf. (Ashutosh)
    
    v7:
    - Use for_each_sysfs_tile_dirfd instead of xe_for_each_tile.
    
    Cc: Aravind Iddamsetty <aravind.iddamsetty@intel.com>
    Cc: Upadhyay <tejas.upadhyay@intel.com>
    Cc: Janga Rahul Kumar <janga.rahul.kumar@intel.com>
    Signed-off-by: Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>
    Reviewed-by: Ashutosh Dixit <ashutosh.dixit@intel.com>

---
+++ tests/xe/xe_sysfs_tile.c
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2023 Intel Corporation
+ */
+
+/**
+ * TEST: Verify physical_vram_size_bytes of each tiles
+ * SUBTEST: physical_vram_size_bytes
+ * Description:
+ *             Read sysfs entry for physical_vram_size_bytes and compare with
+ *             vram size. physical_vram_size_bytes should be more than vram size.
+ */
+
+#include <string.h>
+#include <sys/time.h>
+
+#include "igt.h"
+#include "igt_sysfs.h"
+
+#include "xe_drm.h"
+#include "xe/xe_ioctl.h"
+#include "xe/xe_query.h"
+
+static void test_vram_physical_vram_size_bytes(int tile_fd, int tile_num, u64 vram_size)
+{
+	u64 physical_vram_size_bytes;
+
+	igt_assert(igt_sysfs_scanf(tile_fd, "physical_vram_size_bytes",
+				   "%lx", &physical_vram_size_bytes) > 0);
+	igt_assert_lt_u64(vram_size, physical_vram_size_bytes);
+}
+
+igt_main
+{
+	int fd, tilefd, tile;
+	u64 vram_size;
+
+	igt_fixture {
+		fd = drm_open_driver(DRIVER_XE);
+		xe_device_get(fd);
+	}
+
+	igt_subtest("physical_vram_size_bytes") {
+		igt_require(xe_has_vram(fd));
+		for_each_sysfs_tile_dirfd(fd, tilefd, tile) {
+			vram_size = xe_vram_size(fd, tile);
+			test_vram_physical_vram_size_bytes(tilefd, tile, vram_size);
+		}
+	}
+
+	igt_fixture {
+		xe_device_put(fd);
+		close(fd);
+	}
+}
